26c31a547d9e85303d03952817e5095122d43964
Marcus Järveskog <abr.metalcore98@gmail.com>
2022-01-17 15:16:02 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import Login from './components/Login';
+import "./App.css";
+import Login from "./components/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Login />
+		</div>
+	);
 }
 
 export default App;
