897ec0d09b202b1771efac3d25dbf0b6fd30611b
Filip <filip.sundholm@utb.ecutbildning.se>
2022-01-18 11:00:08 +0100

    conflict change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import Login from './components/Login';
 import {
@@ -18,22 +17,6 @@ function App() {
       </Router>
     </div>
   );
-=======
-import "./App.css";
-import Login from "./components/Login";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-function App() {
-	return (
-		<div className="App">
-			<Router>
-				<Routes>
-					<Route exact path="/" element={<Login />} />
-				</Routes>
-			</Router>
-		</div>
-	);
->>>>>>> 90cc71d24119226864279469eb3b61883f3b9c15
 }
 
 export default App;
