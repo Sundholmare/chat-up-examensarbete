f6ad3fcf2c8f019d1aaf26b3792b6c74fc42bb1f
Marcus Järveskog <abr.metalcore98@gmail.com>
2022-01-19 15:24:06 +0100

    navstyling etc

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,9 +2,36 @@ import Navbar from "./Navbar";
 
 const HomePage = () => {
 	return (
-		<div className="body-height bg-slate-400">
-			<h1>Welcome</h1>
-			<p>insert stuff here</p>
+		<div className="flex body-height bg-slate-400">
+			<aside className="h-full bg-orange-500 w-96">
+				<ul>
+					<li className="px-3 py-5 m-3 text-center bg-white rounded-md nav-shadow">
+						Chatt n stuff
+					</li>
+					<li className="px-3 py-5 m-3 text-center bg-white rounded-md">
+						Chatt n stuff
+					</li>
+					<li className="px-3 py-5 m-3 text-center bg-white rounded-md">
+						Chatt n stuff
+					</li>
+					<li className="px-3 py-5 m-3 text-center bg-white rounded-md">
+						Chatt n stuff
+					</li>
+					<li className="px-3 py-5 m-3 text-center bg-white rounded-md">
+						Chatt n stuff
+					</li>
+					<li className="px-3 py-5 m-3 text-center bg-white rounded-md">
+						Chatt n stuff
+					</li>
+					<li className="px-3 py-5 m-3 text-center bg-white rounded-md">
+						Chatt n stuff
+					</li>
+					<li className="px-3 py-5 m-3 text-center bg-white rounded-md">
+						Chatt n stuff
+					</li>
+				</ul>
+			</aside>
+			<main className="w-full bg-gray-50"></main>
 		</div>
 	);
 };
