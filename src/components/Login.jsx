d6b9d63d9b9867942f8ea04f79f0eee0128ae66e
Filip <filip.sundholm@utb.ecutbildning.se>
2022-01-20 10:46:13 +0100

    fixed login css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,11 +16,11 @@ const Login = () => {
     
     return (
         <div className="bg-main-peach h-screen flex justify-center items-center">
-            <div className="flex flex-col justify-around items-center rounded-2xl bg-off-white w-1/3 h-2/5">
+            <div className="flex flex-col justify-around items-center rounded-2xl bg-off-white w-4/5 lg:w-1/3 h-2/5">
                 <h1 className="font-bold text-4xl opacity-60">Chat-Up</h1>
                 <div 
                 onClick={() => handleLogin("/chat")} 
-                className="flex items-center justify-between w-56 bg-white rounded-md p-2 cursor-pointer onHover shadowBox">
+                className="flex items-center justify-between w-48 md:w-60 bg-white rounded-md p-2 cursor-pointer onHover shadowBox">
                     <img className="h-8" src={google} alt="" />
                     <h2 className="font-bold text-xl opacity-60" >Sign in with Google</h2>
                 </div>
