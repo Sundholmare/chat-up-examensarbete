42de705b8235b6673b9d352314f849768ba2d64d
Marcus Järveskog <abr.metalcore98@gmail.com>
2022-01-26 12:21:35 +0100

    username in nav and nav heigth adjustment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import chatIcon from "../images/chat+icon-1320184411998302345.png";
 import pfp from "../images/pfp.jpeg";
 
-const Navbar = ({ children }) => {
+const Navbar = ({ children, user }) => {
+	console.log(user);
 	return (
 		<div>
 			<div className="relative z-10 flex items-center justify-between px-5 bg-main-peach nav-height nav-shadow">
@@ -9,7 +10,9 @@ const Navbar = ({ children }) => {
 					<img src={chatIcon} className="rounded-full" alt="chat-logo" />
 				</div>
 				<div className="flex items-center">
-					<h3 className="mr-4 font-semibold text-white">AnonymousWeeb69</h3>
+					<h3 className="mr-4 font-semibold text-white">
+						{user && user.displayName}
+					</h3>
 					<div className="w-16 overflow-hidden rounded-full">
 						<img src={pfp} alt="" />
 					</div>
